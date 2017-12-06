41e45152f99662464405c2ebe13478779c5e0a84
jpdmostwanted <32849868+jpdmostwanted@users.noreply.github.com>
2017-12-06 21:16:22 +0530

    added JWT token without db. Hardcoded username and password

---
+++ bookMyMandap/Controllers/TokenController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using bookMyMandap.Models.JWT;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace bookMyMandap.Controllers
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/Token")]
+    public class TokenController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("Test")]
+        public IActionResult Test()
+        {
+            return Ok("Super secret content, I hope you've got clearance for this...");
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult RequestToken([FromBody] TokenRequest request)
+        {
+            if (request.Username == "jpdmw" && request.Password == "password")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.Username)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "bookmymandap.com",
+                    audience: "bookmymandap.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }
+    }
+}
--- bookMyMandap/Startup.cs
+++ bookMyMandap/Startup.cs
@@ -1,28 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace bookMyMandap
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            })
+               .AddJwtBearer(options =>
+               {
+                  
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateLifetime = true,
+                       ValidateIssuerSigningKey = true,
+                       ValidIssuer = "bookmymandap.com",
+                       ValidAudience = "bookmymandap.com",
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]))
+                   };
+               });
+         
             services.AddMvc();
         }
 
@@ -33,7 +58,7 @@ namespace bookMyMandap
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
